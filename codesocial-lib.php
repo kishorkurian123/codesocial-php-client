0fada575758e55bf75eadf3de0d69e9b022b0bed
kishorkurian123 <kishorkurian123@gmail.com>
2015-06-08 12:56:16 +0530

    Update codesocial-lib.php

--- codesocial-lib.php
+++ codesocial-lib.php
@@ -1,4 +1,23 @@
 <?php
-class CodeSocial (){
-    const VERSION = 'v1';
-    const HOST = 'http://167.114.158.94:4000';
+class CodeSocial ($version,$host,$token){
+ public function __construct($token){
+    const VERSION = $version;
+    const URL = $host;
+    const TOKEN = $token;
+ }
+ private function call($type="GET",$data="NULL"){
+     $s = curl_init();
+         curl_setopt($s,CURLOPT_URL,);
+         curl_setopt($s,CURLOPT_HTTPHEADER,array('Expect:'));
+         curl_setopt($s,CURLOPT_TIMEOUT,$this->_timeout);
+         curl_setopt($s,CURLOPT_MAXREDIRS,$this->_maxRedirects);
+         curl_setopt($s,CURLOPT_RETURNTRANSFER,true);
+         curl_setopt($s,CURLOPT_FOLLOWLOCATION,$this->_followlocation);
+         curl_setopt($s,CURLOPT_COOKIEJAR,$this->_cookieFileLocation);
+         curl_setopt($s,CURLOPT_COOKIEFILE,$this->_cookieFileLocation); 
+ }
+ private function getbalance($decode="array"){
+     $response = file_get_contents()
+ }
+ 
+ }
